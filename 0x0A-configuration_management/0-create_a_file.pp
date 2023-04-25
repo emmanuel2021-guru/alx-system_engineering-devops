4fccbb205ae4dd5d25bd1fab83f42c307b2431f0
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-25 18:07:49 +0100

    Fixed code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This script creates a file in /tmp/ with some requirements
-school { '/tmp/school':
-  ensure => '/tmp/school'
-  group => 'www-data'
-  owner => 'www-data'
-  mode => '0744'
+file { '/tmp/school':
+  ensure => 'present',
+  group => 'www-data',
+  owner => 'www-data',
+  mode => '0744',
   content => 'I love Puppet'
 }
