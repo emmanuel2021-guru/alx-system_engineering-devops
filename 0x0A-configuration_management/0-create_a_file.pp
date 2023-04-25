f82be9063663704ba4cdec2e2fd1b228f1bbe66e
Emmanuel <emmanuellawakin2005@gmail.com>
2023-04-25 18:12:54 +0100

    Fixed puppet-lint error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This script creates a file in /tmp/ with some requirements
 file { '/tmp/school':
-  ensure => 'present',
-  group => 'www-data',
-  owner => 'www-data',
-  mode => '0744',
+  ensure  => 'present',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744',
   content => 'I love Puppet'
 }
