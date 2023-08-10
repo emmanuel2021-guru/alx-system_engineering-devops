84ce25ba4d2003f1008059ac087e3c8e3be136af
Emmanuel <emmanuellawakin2005@gmail.com>
2023-08-10 23:23:37 +0100

    Made the URLs more readable

--- 0x15-api/1-export_to_csv.py
+++ 0x15-api/1-export_to_csv.py
@@ -10,10 +10,10 @@ import sys
 
 if __name__ == "__main__":
     r_user_tasks = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}/todos'
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
             .format(sys.argv[1]))
     r_user_info = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}'
+            'https://jsonplaceholder.typicode.com/users?id={}'
             .format(sys.argv[1]))
     user_tasks = json.loads(r_user_tasks._content)
     user_info = json.loads(r_user_info._content)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         if tasks['completed'] is True:
             completed_tasks += 1
     print("Employee {} is done with tasks({}/{}):"
-          .format(user_info['name'],
+          .format(user_info[0]['name'],
                   completed_tasks, len(user_tasks)))
     for tasks in user_tasks:
         if tasks['completed'] is True:
