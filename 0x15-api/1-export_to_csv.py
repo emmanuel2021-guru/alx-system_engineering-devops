8ca13a02974bbb5d85ce83f2ff8713329adb43d0
Emmanuel <emmanuellawakin2005@gmail.com>
2023-08-11 02:06:22 +0100

    Completed task 1

--- 0x15-api/1-export_to_csv.py
+++ 0x15-api/1-export_to_csv.py
@@ -6,24 +6,27 @@ and returns information about his/her TODO list progress"""
 import json
 import requests
 import sys
+import csv
 
 
 if __name__ == "__main__":
     r_user_tasks = requests.get(
-            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            'https://jsonplaceholder.typicode.com/users/{}/todos'
             .format(sys.argv[1]))
     r_user_info = requests.get(
-            'https://jsonplaceholder.typicode.com/users?id={}'
+            'https://jsonplaceholder.typicode.com/users/{}'
             .format(sys.argv[1]))
     user_tasks = json.loads(r_user_tasks._content)
     user_info = json.loads(r_user_info._content)
-    completed_tasks = 0
-    for tasks in user_tasks:
-        if tasks['completed'] is True:
-            completed_tasks += 1
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user_info[0]['name'],
-                  completed_tasks, len(user_tasks)))
-    for tasks in user_tasks:
-        if tasks['completed'] is True:
-            print("\t {}".format(tasks['title']))
+    rows = []
+    for task in user_tasks:
+        row = []
+        row.append('{}'.format(user_info['name']))
+        row.append('{}'.format(user_info['username']))
+        row.append('{}'.format(task['completed']))
+        row.append('{}'.format(task['title']))
+        rows.append(row)
+    filename = "{}.csv".format(sys.argv[1])
+    with open(filename, 'w', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        csvwriter.writerows(rows)
